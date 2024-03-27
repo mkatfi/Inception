86ad95c67e4a97d4b2892bc4a87830950eae8dd4
mkatfi <mkatfi@student.1337.ma>
2024-03-27 01:55:55 +0000

    onepart

--- Inception/srcs/requirements/mariadb/script.sh
+++ Inception/srcs/requirements/mariadb/script.sh
@@ -1,20 +1,32 @@
 
+# Start the MariaDB service
 service mariadb start
+# Wait for the database to be ready
+until mysqladmin ping -h "localhost" -u"root" ; do
+    echo "Waiting for MariaDB to be ready..."
+    sleep 5
+done
 
-sleep 5
-
+# Create a database if it doesn't already exist
 mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 
-mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+# Create a user that can connect from any host
+mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
+# Grant all privileges on the specified database to the user
 mariadb -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
+# Change the password for the root user
 mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 
+# Grant all privileges on the specified database to the root user
 mariadb -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 
+# Reload the privileges to apply any changes made
 mariadb -u root -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 
+# Shut down the MariaDB server gracefully
 mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
 
+# Start the MariaDB server in safe mode
 mysqld_safe
--- Inception/srcs/requirements/wordpress/auto_config.sh
+++ Inception/srcs/requirements/wordpress/auto_config.sh
@@ -1,4 +1,10 @@
+
 sed -i -e 's/.*listen = .*/listen = 9000/' /etc/php/7.4/fpm/pool.d/www.conf
+
+until mariadb -h "$DB_host"  -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e ";"  ; do
+	echo "Waiting for MariaDB to be ready..."
+	sleep 5
+done
 
 wp core download --allow-root
 
@@ -8,9 +14,12 @@ wp config create --allow-root \
                  --dbname=$MYSQL_DATABASE \
 				 --dbuser=$MYSQL_USER \
 				 --dbpass=$MYSQL_PASSWORD \
-				 --dbhost=mariadb:3306 --path='/var/www/wordpress'
+				 --dbhost=$DB_host 
 
-wp core install --url=mkatfi.42.fr --title=baradosa --admin_user=barados --admin_password=123 --admin_email=info@gmail.com --allow-root
-wp user create bradisa bradisa@gamail.com --role=editor --user_pass=123 --allow-root
-
+wp core install --url=$DOMUNE_NAME \
+				--title=$TITER \
+				--admin_user=$ADMIN_USER \
+				--admin_password=$ADMIN_PASSWORD \
+				--admin_email=$EMAIL --allow-root
+wp user create bradisa bradisa@gamail.com --role=$ROLE --user_pass=$USER_PASS --allow-root
 /usr/sbin/php-fpm7.4 -F
